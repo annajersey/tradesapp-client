e40ec41406662104a7e45314214633fdca9cb292
anna <anna.bogomiagkova@codeit.com.ua>
2018-08-31 17:21:54 +0300

    get symbols with our api

--- src/App.js
+++ src/App.js
@@ -1,58 +1,76 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import socketIOClient from "socket.io-client";
+import axios from "axios";
+
 class App extends Component {
     constructor() {
         super();
         this.state = {
             response: {},
             endpoint: "http://localhost:5001",
-
         };
-        this.symbols = ['ETHBTC', 'BTCUSDT', 'ETHUSDT']
+        this.symbols = ['ETHBTC', 'BTCUSDT', 'ETHUSDT', 'KEYETH', 'NASBTC', 'NASETH',
+            'NASBNB', 'MFTBTC', 'MFTETH', 'MFTBNB']
 
     }
+
     componentDidMount() {
-        const { endpoint } = this.state;
-        const socket = socketIOClient(endpoint+'?symbols='+this.symbols.join());
+        const {endpoint} = this.state;
+
+        axios.get(endpoint + '/symbols')
+            .then(response => {
+                console.log(response.data);
+                this.symbols = {};
+                for (let i in response.data) {
+                    this.symbols[response.data[i].symbol] = response.data[i].baseAsset + '/' + response.data[i].quoteAsset;
+                }
+
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+        const socket = socketIOClient(endpoint + '?symbols=' + this.symbols.join());
         socket.on("TradesAPI", data => {
             let result = JSON.parse(data)
             let response = {...this.state.response, [result.symbol]: result};
-            this.keys=Object.keys(result);
+            this.keys = Object.keys(result);
             this.setState({response})
         });
     }
+
     render() {
-        const { response } = this.state;
+        const {response} = this.state;
         console.log(response)
-        if(Object.keys(response).length){
+        if (Object.keys(response).length) {
             let resultObject = {...this.state.response}
-            let result=[]
-            for(var i in resultObject){
-                for(var j in  resultObject[i]){
-                    result[j] ? result[j].push(resultObject[i][j]) : result[j]=[resultObject[i][j]]
+            let result = []
+            for (var i in resultObject) {
+                for (var j in  resultObject[i]) {
+                    result[j] ? result[j].push(resultObject[i][j]) : result[j] = [resultObject[i][j]]
                 }
             }
-
-            console.log('result',result.bestAsk);
-        return (
-            <div>
-                <h2>Response</h2>
+            return (
                 <div>
-                    <table>
-                        {Object.keys(result).map((key,index) => {
-                            return <tr className={index%2?'odd':'even'}><td>{key}</td>{result[key].map(item => <td>{item}</td>)}</tr>
-                        })
+                    <h2>Response</h2>
+                    <div>
+                        <table>
+                            {Object.keys(result).map((key, index) => {
+                                return <tr className={index % 2 ? 'odd' : 'even'}>
+                                    <td>{key}</td>
 
-                        }
-
-					</table>
+                                    {result[key].map(item => <td>{key === 'symbol' ? this.symbols[item] : item}</td>)}
+                                </tr>
+                            })
+                            }
+                        </table>
+                    </div>
                 </div>
-
-
-            </div>
-        )}else{
+            )
+        } else {
             return 'Loading...'
         }
     }
 }
+
 export default App;
